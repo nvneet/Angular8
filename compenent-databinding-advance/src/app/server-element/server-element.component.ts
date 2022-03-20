a93d8c2e1e06ea6a6081d3b13e5f51d7b068b0b5
nvneet <navneet.ism@gmail.com>
2022-03-21 01:22:34 +0530

    component property event data binding advance

---
+++ compenent-databinding-advance/src/app/server-element/server-element.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, ShadowDom
+})
+export class ServerElementComponent implements OnInit {
+
+  // @Input() element: {type: string, name: string, content: string} = {type:'nav', name:'new', content:'light weight'};
+  @Input('srvElement') element: {type: string, name: string, content: string} = {type:'nav', name:'new', content:'light weight'};
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}

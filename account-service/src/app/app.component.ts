de50ea0f62ca42d8bbcd45912771710b793acb69
nvneet <navneet.ism@gmail.com>
2022-04-03 05:49:01 +0530

    service injection at app module level for @Injectable

--- account-service/src/app/app.component.ts
+++ account-service/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountsService } from './accounts.services';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountsService]
+  // providers: [LoggingService,AccountsService] //services are provided at app-module level
 })
 export class AppComponent implements OnInit{
 
